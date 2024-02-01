0fad49d37a1c53040d468108199d39586a562fa9
ColdPark <ekzm103@gmail.com>
2024-02-02 07:55:11 +0900

    Prefab Import

--- Assets/Script/EnemyMover.cs
+++ Assets/Script/EnemyMover.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
+    [SerializeField] private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        PirntWayPointName();
+        StartCoroutine(PirntWayPointName());
     }
 
-    void PirntWayPointName()
+    IEnumerator PirntWayPointName()
     {
         foreach(WayPoint wayPoint in path)
         {
-            Debug.Log(wayPoint.name);
+            //Debug.Log(wayPoint.name);
+            transform.position = wayPoint.transform.position;
+            yield return new WaitForSeconds(timer);
         }
     }
 }
