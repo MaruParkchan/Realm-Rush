35325fdfc5a6764c84d2b3744dd70054e9a4471f
ColdPark <ekzm103@gmail.com>
2024-02-02 07:31:56 +0900

    Waypoint Add
    Waypoint , Enemy 추가

--- Assets/Script/CoordinateLabeler.cs
+++ Assets/Script/CoordinateLabeler.cs
@@ -17,21 +17,21 @@ public class CoordinateLabeler : MonoBehaviour
 
     private void Update()
     {
-        if (!Application.isPlaying)
+        if (!Application.isPlaying) // 플레이중 아닐때 업데이트 
         {
             DisPlayCoordinates();
             UpdateObjectName();
         }
     }
 
-    void DisPlayCoordinates()
+    void DisPlayCoordinates() // 좌표 설정
     {
         coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
         coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
         label.text = coordinates.x + "," + coordinates.y;
     }
 
-    void UpdateObjectName()
+    void UpdateObjectName() // 오브젝트 이름 좌표 실시간 바꾸기 
     {
         transform.parent.name = coordinates.ToString();
     }
