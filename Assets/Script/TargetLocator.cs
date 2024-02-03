bdb7f5131265d4d825ddfb20af2d9868ff71c2fd
ColdPark <ekzm103@gmail.com>
2024-02-03 19:20:35 +0900

    가장 가까운 적 타워 공격
    적과 타워의 거리계산을 통해 공격구현

--- Assets/Script/TargetLocator.cs
+++ Assets/Script/TargetLocator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] private Transform weapon;
+    [SerializeField] private ParticleSystem projectileParticles;
+    [SerializeField] private float range = 15.0f;
     private Transform target;
 
     void Start()
@@ -13,10 +15,45 @@ public class TargetLocator : MonoBehaviour
     }
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+            target = closestTarget;
+        }
+
     }
     private void AimWeapon() // 적 방향으로 회전
     {
+        float targetDistance = Vector3.Distance(transform.position, target.transform.position);
         transform.LookAt(target);
+        if(targetDistance < range)
+        {
+            Attack(true);
+        }
+        else 
+        {
+            Attack(false);
+        }
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
