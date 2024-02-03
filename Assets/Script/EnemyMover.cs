09725903b928f4754faab32569b2344ace2b4743
ColdPark <ekzm103@gmail.com>
2024-02-03 13:06:16 +0900

    Enemy의 해당 경로 이동 구현
    Vector3.Lerp 사용

--- Assets/Script/EnemyMover.cs
+++ Assets/Script/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] private float timer;
+    [SerializeField] [Range(0.0f, 5.0f)] private float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +14,19 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator PirntWayPointName()
     {
-        foreach(WayPoint wayPoint in path)
+        foreach (WayPoint wayPoint in path)
         {
-            //Debug.Log(wayPoint.name);
-            transform.position = wayPoint.transform.position;
-            yield return new WaitForSeconds(timer);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = wayPoint.transform.position;
+            float travelpercent = 0.0f;
+
+            transform.LookAt(endPosition);
+            while (travelpercent < 1.0f)
+            {
+                travelpercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelpercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
