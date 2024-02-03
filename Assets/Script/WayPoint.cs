b1e72c91d385cbd04ced1ed61d14458f9ce25273
ColdPark <ekzm103@gmail.com>
2024-02-03 14:29:09 +0900

    마우스 입력 탐지
    Waupoint 수정

--- Assets/Script/WayPoint.cs
+++ Assets/Script/WayPoint.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool isPlaceable;
+    private void OnMouseOver()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isPlaceable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
