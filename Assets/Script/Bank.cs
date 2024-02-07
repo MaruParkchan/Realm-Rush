0c5b1652f1bf7db333b40e999553e4912359b21a
ColdPark <ekzm103@gmail.com>
2024-02-07 18:58:25 +0900

    Currency System 완성
    적 오브젝트 파괴시 돈 획득, 돈이 -될시 씬 재로드

--- Assets/Script/Bank.cs
+++ Assets/Script/Bank.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int startingBalance = 150;
 
     [SerializeField] int currentBalance;
-    public int CurrentBalance { get { return currentBalance;}}
-    
+    public int CurrentBalance { get { return currentBalance; } }
 
-    private void Awake() 
+
+    private void Awake()
     {
-        
+        currentBalance = startingBalance;
     }
 
-    public void Deposit(int amount)
+    public void Deposit(int amount) // 보증금, 잔고
     {
         currentBalance += Mathf.Abs(amount); // 절대값 바꾸기
     }
 
-    public void WithDraw(int amount)
+    public void WithDraw(int amount) // 빼기
     {
         currentBalance -= Mathf.Abs(amount);
+
+        if (currentBalance <= -1)
+        {
+            ReloadScene();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
--- Assets/Script/WayPoint.cs
+++ Assets/Script/WayPoint.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private Tower towerPrefab;
     [SerializeField] private bool isPlaceable;
     public bool IsPlaceable { get { return isPlaceable;} }
 
     private void OnMouseDown() // IsPlaceable는 해당 블럭위에 타워를 설치해도 되는지?
     {
-        Debug.Log("ㅇㅇ");
         if (isPlaceable)
         {
-            Instantiate(towerPrefab, transform.position, Quaternion.identity);
-            isPlaceable = false;
+            bool isPlaced =  towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
         }
     }
 }
