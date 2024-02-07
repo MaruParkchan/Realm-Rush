e36624d95641912d813ca58fdb2bd74b1384791a
ColdPark <ekzm103@gmail.com>
2024-02-07 19:36:52 +0900

    UI Text 설정완료
    TextMeshPro로 설정

--- Assets/Script/Bank.cs
+++ Assets/Script/Bank.cs
@@ -2,33 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int startingBalance = 150;
-
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
+
+    [SerializeField] TextMeshProUGUI displayBalance;
 
 
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount) // 보증금, 잔고
     {
         currentBalance += Mathf.Abs(amount); // 절대값 바꾸기
+        UpdateDisplay();
     }
 
     public void WithDraw(int amount) // 빼기
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (currentBalance <= -1)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     private void ReloadScene()
